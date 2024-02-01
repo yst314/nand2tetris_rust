7d3909e98c0eecbd478467abd135fc85909b54e5
Yoshito Osanai <yoshito-osanai@mes.co.jp>
2024-02-01 18:01:58 +0900

    not16

--- src/lib.rs
+++ src/lib.rs
@@ -21,17 +21,35 @@ pub fn xor(a: bool, b: bool) -> bool {
 pub fn mux(a: bool, b: bool, sel: bool) -> bool {
     or(and(a, not(sel)), and(b, sel))
 }
-pub fn dmux(input:bool, sel:bool) -> [bool; 2]{
+pub fn dmux(input: bool, sel: bool) -> [bool; 2] {
     [and(not(sel), input), and(sel, input)]
 }
 
-// pub fn not16(a:[bool; 16]) -> [bool; 16]{
-//     not(a[0])
-// }
+pub fn not16(a: [bool; 16]) -> [bool; 16] {
+    let mut output = [false; 16];
+    for i in 0..16 {
+        output[i] = not(a[i]);
+    }
+    output
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_not16() {
+        let input = [
+            false, false, false, false, false, false, false, false, false, false, false, false,
+            false, false, false, false,
+        ];
+
+        let output = [
+            true, true, true, true, true, true, true, true, true, true, true, true, true, true,
+            true, true,
+        ];
+        assert_eq!(not16(input), output);
+    }
 
     #[test]
     fn test_nand() {
@@ -73,21 +91,21 @@ mod tests {
 
     #[test]
     fn test_mux() {
-        assert_eq!(mux(false, false,  false), false);
-        assert_eq!(mux(false, true ,  false), false);
-        assert_eq!(mux(true,  false , false), true);
-        assert_eq!(mux(true,  true  , false), true);
-        assert_eq!(mux(false, false,  true),  false);
-        assert_eq!(mux(false, true ,  true),  true);
-        assert_eq!(mux(true,  false , true),  false);
-        assert_eq!(mux(true,  true  , true),  true);
+        assert_eq!(mux(false, false, false), false);
+        assert_eq!(mux(false, true, false), false);
+        assert_eq!(mux(true, false, false), true);
+        assert_eq!(mux(true, true, false), true);
+        assert_eq!(mux(false, false, true), false);
+        assert_eq!(mux(false, true, true), true);
+        assert_eq!(mux(true, false, true), false);
+        assert_eq!(mux(true, true, true), true);
     }
 
     #[test]
     fn test_dmux() {
         assert_eq!(dmux(false, false), [false, false]);
-        assert_eq!(dmux(false, true),  [false, false]);
-        assert_eq!(dmux(true, false),  [true, false]);
-        assert_eq!(dmux(true, true),   [false, true]);
+        assert_eq!(dmux(false, true), [false, false]);
+        assert_eq!(dmux(true, false), [true, false]);
+        assert_eq!(dmux(true, true), [false, true]);
     }
 }
